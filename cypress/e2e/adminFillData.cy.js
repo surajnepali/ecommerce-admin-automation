d8d8b08f368b791baa08d51feff63fd5288340d7
Suraj Nepali <surajnepali7896@gmail.com>
2023-02-22 15:20:58 +0545

    add: env url

--- cypress/e2e/adminFillData.cy.js
+++ cypress/e2e/adminFillData.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit(Cypress.env('url'))
-  })
-})
+describe('To add category, head category, sub category, image, and product', () => {
+  it('should convert Ecel file to .json file', () => {});
+
+  it('should add three categories', () => {
+    cy.visit(Cypress.env('url'));
+  });
+});
